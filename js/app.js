6ebf8c5722deaa63e6c9e37051529385c86ba4eb
Mili Antoni <148805721+Mili30082003@users.noreply.github.com>
2025-01-24 17:24:30 -0300

    Hice la seccion Ofertas, brasil y grid

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Seleccionar los elementos del DOM
+const backBtn = document.querySelector('.backBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const productContainer = document.querySelector('.product-container');
+
+// Agregar los event listeners para el desplazamiento
+backBtn.addEventListener('click', () => {
+    productContainer.scrollBy({
+        top: 0,
+        left: -300,
+        behavior: 'smooth'
+    });
+});
+
+nextBtn.addEventListener('click', () => {
+    productContainer.scrollBy({
+        top: 0,
+        left: 300,
+        behavior: 'smooth'
+    });
+});
